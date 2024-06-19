582503e988e2f8e143d2da396e2433b63b1df995
brunogmatias <bruno.gabriel0589@gmail.com>
2024-06-19 15:04:56 -0300

    criei os ex009 e 010

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel}KM`)
+if(vel > 60){
+    console.log('Você está acima do limite de velocidade!')
+}else{
+console.log('Você está abaixo do limite de velocidade')}
