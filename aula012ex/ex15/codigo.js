e0e67fcc2320d040526523aa81b7c1dff95d7286
brunogmatias <bruno.gabriel0589@gmail.com>
2024-07-17 14:59:29 -0300

    atualizei o ex12

--- aula012ex/ex15/codigo.js
+++ aula012ex/ex15/codigo.js
@@ -4,48 +4,52 @@ function verificar(){
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
     var txtp = document.getElementById('txtp')
-
-    if(fano.value.lenght == 0 || fano.value > ano){
+    var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        
+    if(fano.value > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ' '
-        var img = document.createElement('img')
-        img.setAttribute('id','foto')
+        
         if(fsex[0].checked){
             genero = 'Homem'
             if(idade >=0 && idade < 18){
                 //criança
                 img.setAttribute('src','imagens/h.crianca.png')
-            }else if(idade < 21){
+            }else if(idade > 18 && idade < 25){
                 //jovem
                 img.setAttribute('src','imagens/j.homem.png')
-            }else if(idade < 50){
+            }else if(idade > 25 && idade < 50){
                 //adulto
                 img.setAttribute('src','imagens/homem40.png')
-            }else{
+            }else if(idade > 50 && idade < 110){
                 //idoso
                 img.setAttribute('src','imagens/idoso.png')
             }
         }
-        if(fsex[1].checked){
+        else if(fsex[1].checked){
             genero = 'Mulher'
-            if(idade >=0 && idade < 18){
+            if(idade >= 0 && idade < 18){
                 //criança
                 img.setAttribute('src','imagens/m.crianca.png')
-            }else if(idade < 21){
+            }else if(idade > 18 && idade < 25){
                 //jovem
-                img.setAttribute('src','imagens/j.mulher,png')
-            }else if(idade < 50){
+                img.setAttribute('src','imagens/j.mulher.png')
+            }else if(idade > 25 && idade < 50){
                 //adulto
                 img.setAttribute('src','imagens/mulher40.png')
-            }else{
+            }else if(idade > 50 && idade < 110){
                 //idoso
                 img.setAttribute('src','imagens/idosa.png')
             }
-        }
-        txtp.innerHTML = (`Detectamos ${genero} com ${idade} anos.`)
-        res.appendChild(img)
+        }       
 }
+    if(Number(fano.value) == 0 || Number(fano.value) == ano){
+        window.alert('Error: adicone o ano de nascimento!')
+    }else{
+    txtp.innerHTML = (`Detectamos ${genero} com ${idade} anos.`)
+    res.appendChild(img)}
 }
