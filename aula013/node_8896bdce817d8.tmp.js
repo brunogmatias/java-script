ae01c7f84a2e62af69910d52974661ea60c6bcd2
brunogmatias <bruno.gabriel0589@gmail.com>
2024-07-30 12:27:01 -0300

    ,

---
+++ aula013/node_8896bdce817d8.tmp.js
@@ -0,0 +1,28 @@
+
+
+/*for(var c=1 ; c <=7 ; c++){
+    console.log(`Passo ${c}`)
+}
+
+
+/*var c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
+
+
+var c = 1
+
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+var n1 = 1
+var n2 = 8
+var passo = 2
+for(var c=n1 ; n1<=n2; c += passo){
+    console.log(`${n1}`)
+}
