95799c48839a5c81d296db7b2c32385887e7ad49
brunogmatias <bruno.gabriel0589@gmail.com>
2024-08-19 18:16:10 -0300

    criei o ex da aula016

---
+++ aula016.funcao/funcao.js
@@ -0,0 +1,25 @@
+/* function parImpar(n){
+    if(n%2 == 0){
+        return 'Par'
+    }else{
+        return 'Ímpar'
+    }
+}
+
+console.log(parImpar(800))
+*/
+
+/*
+function soma(n1=0, n2=0){
+    return n1 + n2
+}
+
+var num = [2,3]
+
+console.log(soma(num[0], num[1]))
+*/
+
+ var s = function(n){
+    return n*2
+ }
+ console.log(s(5))
---
+++ aula016ex/modelo/codigo.js
@@ -0,0 +1,14 @@
+var n = document.getElementById('txtn')
+var sel = [sel]
+
+
+function adicionar(){
+    
+    if(Number(n.value) > 100){
+        window.alert('error')
+    }else{
+        window.alert('certo')
+    }
+}
+
+
